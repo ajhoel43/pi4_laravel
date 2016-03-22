b7e482f7d944fb7146f8c0e3445f19fd703dacf5
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2016-03-22 12:59:21 +0700

    Proses Penambahan Tabel Baru menggunakan Laravel Migration

---
+++ database/migrations/2016_03_22_055510_create_pi4_latihan.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePi4Latihan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nim');
+            $table->string('nama');
+            $table->date('ttl');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('mahasiswa');
+    }
+}
