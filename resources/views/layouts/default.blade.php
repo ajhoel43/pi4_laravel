4e4ad6eb59d1695811a9b7c3cdd1053b55490b87
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2016-04-25 15:04:17 +0700

    change bootstrap source add bootstrap js source

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,10 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+        <!-- <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet"> -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <style>
             table form { margin-bottom: 0; }
             form ul { margin-left: 0; list-style: none; }
